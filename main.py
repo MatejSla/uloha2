ea9d61be392b96056892ff6fd32238b5afecb005
Matejko <matejjslavik@gmail.com>
2023-11-02 19:59:21 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-jhvguzjzuzjvhgfhfhgfhggfhfghhgfghfhfg
+# 13 ka úloha ;)
+cislo = int(input("Zadaj mi číslo:"))
+while cislo !=1:
+  if cislo % 2 == 0:
+    cislo = cislo / 2
+    print(int(cislo))
+  else:
+    cislo = 3 * cislo + 1
+    print(int(cislo))
